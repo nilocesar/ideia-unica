9513d09d0794cdc79365e8fc07c5eee3e9cde5eb
Nilo César <e_maildonilo@yahoo.com.br>
2021-03-03 11:56:12 -0300

    ajuste 2

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Home() {
     return (
         <div> 
-            <h1>Home222</h1>
+            <h1>Nilo César Novo Teste</h1>
             <Contador /> 
         </div>
     )
