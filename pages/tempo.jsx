0156d53633dcd7c414caba415a311c6241b3f61c
Nilo César <e_maildonilo@yahoo.com.br>
2021-03-03 21:30:16 -0300

    ajuste branch main

--- pages/tempo.jsx
+++ pages/tempo.jsx
@@ -1,12 +1,25 @@
-function Tempo(){
+function Tempo(props){
   const dynamicDate = new Date();
   const dynamicDateString = dynamicDate.toGMTString();
 
   return (
     <div>
-      {dynamicDateString}
+      <div>{dynamicDateString} (dinamico)</div>
+      <div>{props.staticDateString} (estatico)</div>
     </div>
   )
+}
+
+export function getStaticProps(){
+  const staticDate = new Date();
+  const staticDateString = staticDate.toGMTString();
+
+  return {
+    props:{
+      staticDateString
+    }
+  }
+
 }
 
 export default Tempo;
